19b5b4d35599792678f81f8c44dc2e72ebef3d65
IrrelevantGaymer <101901cora@gmail.com>
2025-08-15 02:12:50 -0700

    I reverted a bunch of stuff on accident, so I'm very apologetic for the git graph.  This is me fixing some stuff that got lost between reverts and commits

--- src/world_generation/blocks/mod.rs
+++ src/world_generation/blocks/mod.rs
@@ -3,10 +3,7 @@ use bevy::app::Plugin;
 use crate::{
     table, 
     world_generation::blocks::{
-        block::Block, 
-        block_builders::{basic::BasicBuilder, pillar::PillarBuilder}, 
-        buildable::CanBuild, 
-        face::CullingFlag
+        block::Block, block_builders::{basic::BasicBuilder, pillar::PillarBuilder}, block_types::BlockType, buildable::CanBuild, face::CullingFlag
     }
 };        
 
@@ -19,16 +16,23 @@ pub mod define;
 pub mod face;
 pub mod mesh_type;
 pub mod table;
+pub mod texture_builder;
 
-table!(CanBuild, enum BlockId, static BLOCK_BUILDERS = {
-    let Dirt: BasicBuilder = Block::new_basic("dirt");
-    let Grass: BasicBuilder = Block::new_basic("grass_block");
-    let Stone: BasicBuilder = Block::new_basic("stone");
-    let Log: PillarBuilder = Block::new_pillar("log");
-    let Snow: BasicBuilder = Block::new_basic("snow_block");
-    let Leaf: BasicBuilder = Block::new_basic("leaf")
-        .with_culling_flag(CullingFlag::None);
-});
+pub const DEFAULT_TEXTURE: &'static str = "debug_texture.png";
+
+table!(
+    CanBuild<BuildTo = BlockType>, BlockType, 
+    enum BlockId, 
+    static BLOCK_BUILDERS = {
+        let Dirt: BasicBuilder = Block::new_basic("dirt");
+        let Grass: BasicBuilder = Block::new_basic("grass_block");
+        let Stone: BasicBuilder = Block::new_basic("stone");
+        let Log: PillarBuilder = Block::new_pillar("log");
+        let Snow: BasicBuilder = Block::new_basic("snow_block");
+        let Leaf: BasicBuilder = Block::new_basic("leaf")
+            .with_culling_flag(CullingFlag::None);
+    }
+);
 
 pub struct BlocksPlugin;
 
--- src/world_generation/blocks/table.rs
+++ src/world_generation/blocks/table.rs
@@ -1,17 +1,41 @@
-pub struct Table<T: 'static + ?Sized>(pub &'static [&'static T]);
+pub struct BuilderTable<T: 'static + ?Sized>(pub &'static [&'static T]);
 
-impl<T: 'static + ?Sized> Table<T> {
+impl<T: 'static + ?Sized> BuilderTable<T> {
     pub fn iter(&self) -> impl Iterator<Item = &'static T> + '_ {
         self.0.iter().copied()
     }
 }
 
-impl<T: 'static + ?Sized> IntoIterator for &Table<T> {
+impl<T: 'static + ?Sized> IntoIterator for &BuilderTable<T> {
     type Item = &'static T;
 
     type IntoIter = std::iter::Copied<std::slice::Iter<'static, &'static T>>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.0.iter().copied()
+    }
+}
+
+pub struct Table<T>(Box<[T]>);
+
+impl<T> Table<T> {
+    pub fn get(&self, idx: usize) -> Option<&T> {
+        self.0.get(idx)
+    }
+
+    pub fn get_mut(&mut self, idx: usize) -> Option<&mut T> {
+        self.0.get_mut(idx)
+    }
+
+    pub unsafe fn get_unchecked(&self, idx: usize) -> &T {
+        unsafe {
+            self.0.get_unchecked(idx)
+        }
+    }
+
+    pub unsafe fn get_unchecked_mut(&mut self, idx: usize) -> &mut T {
+        unsafe {
+            self.0.get_unchecked_mut(idx)
+        }
     }
 }
